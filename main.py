5d28eb65ecf7eaf7383746ef364c967bfc385207
oilers <gdss@yandex.ru>
2023-03-06 14:47:05 +0300

    ready beck

--- main.py
+++ main.py
@@ -1,29 +1,34 @@
-def range_ip (start, finish):
-    """Принимает старт диапозона и финиш. Генерирует  все IP в диапозоне. Создает и Записывает
-    их в Файл.тхт (про запись в фаил временное решение"""
-    from netaddr import iter_iprange
-    generator = iter_iprange(start, finish, step=1)
-    my_file = open('output.txt', 'w')
-    for ipa in generator:
-        my_file.write(str(ipa))
-        my_file.write("\n")
-    my_file.close()
+def range_ip(start, finish, port):
+    """Принимает старт диапозона и финиш. Генерирует  все IP в диапозоне
+    добавляет порт"""
+    import ipaddress
+    range_ipa = []
+    start_ip = ipaddress.IPv4Address(start)
+    end_ip = ipaddress.IPv4Address(finish)
+    for ip_int in range(int(start_ip), int(end_ip)):
+        range_ipa.append(str(ipaddress.IPv4Address(ip_int)))
+    ip_port = []
+    for ip in range_ipa:
+        ip_port.append(ip + ":" + port)
+    return ip_port
+
+
+def withe_lest(server_work):
+    """Принимает переменную со списком IP и портами перебирает их
+    записывает в файл с указанием версии"""
+    from mcstatus import JavaServer
+    z = []
+    for i in server_work:
+        try:
+            server = JavaServer.lookup(i)
+            status = server.status()
+            if server.ping() > 1:
+                print(i, status.version.name)
+                z.append(i + " " + status.version.name)
+        except Exception:
+            print(i, "No")
+    print(z)
 
 
 
-
-def port_open():
-    """Считывает построчно из файла IP проверяет на открытый порт. Возврат файл с IP
-    адресами открытых портов"""
-
-
-
-def parser(ip_port):
-    """Принимет IP с открытым портом проверяет на наличие сервера """
-    pass
-
-def withe_lest(server_ip):
-    """Принимает сервера с IP и портами проверяет конфиг на ВайтЛист, записывает
-    в файл с указанием версии"""
-    pass
-range_ip('5.8.62.0', '5.8.62.255')
+withe_lest(range_ip('79.175.1.0', '79.175.3.255', "25565"))
